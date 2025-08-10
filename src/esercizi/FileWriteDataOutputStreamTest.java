456868383b45b276c6a88308c5eb6dd5ad72c81a
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-08-10 22:11:04 +0200

    testWriteOnFile() method done - JUnit

--- src/esercizi/FileWriteDataOutputStreamTest.java
+++ src/esercizi/FileWriteDataOutputStreamTest.java
@@ -3,6 +3,9 @@ package esercizi;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,9 +16,15 @@ import org.junit.jupiter.api.Test;
 class FileWriteDataOutputStreamTest {
 	
 	static FileWriteDataOutputStream fwdos;
-	final String fileToWriteName = "src/text_files/fatturaNonChar.dat";
-	final String fileToReadName = "src/text_files/fatturaNonChar.dat";
+	final String invoiceFile = "src/text_files/fatturaNonChar.dat";
 	static record Article(String productName, double price, int units) {}
+	List<esercizi.FileWriteDataOutputStream.Article> articleList = List.of(
+			new FileWriteDataOutputStream.Article("T-shirt", 19.99, 12),
+			new FileWriteDataOutputStream.Article("Mug", 9.99, 8),
+			new FileWriteDataOutputStream.Article("Duke", 15.99, 13),
+			new FileWriteDataOutputStream.Article("Pin", 3.99, 29),
+			new FileWriteDataOutputStream.Article("Key-Chain", 4.99, 50)
+	);
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -25,21 +34,20 @@ class FileWriteDataOutputStreamTest {
 
 	@Test
 	void testReadWritedFile() {
-		
-		List<esercizi.FileWriteDataOutputStream.Article> expArtList = List.of(
-				new FileWriteDataOutputStream.Article("T-shirt", 19.99, 12),
-				new FileWriteDataOutputStream.Article("Mug", 9.99, 8),
-				new FileWriteDataOutputStream.Article("Duke", 15.99, 13),
-				new FileWriteDataOutputStream.Article("Pin", 3.99, 29),
-				new FileWriteDataOutputStream.Article("Key-Chain", 4.99, 50)
-		);
-		
-		assertArrayEquals(expArtList.toArray(), fwdos.readWritedFile(fileToReadName).toArray());
+		assertArrayEquals(articleList.toArray(), fwdos.readWritedFile(invoiceFile).toArray());
 	}
 	
 	@Test
-	void testWriteOnFile() {
+	void testWriteOnFile() throws IOException {
 		
+		// Svuoto file
+		Files.write(Paths.get("src/text_files/fatturaNonChar.dat"), new byte[0]);
+		
+		// Scrivo il file
+		fwdos.writeOnFile(invoiceFile);
+		
+		// Leggo il file e confronto
+		assertArrayEquals(articleList.toArray(), fwdos.readWritedFile(invoiceFile).toArray());
 	}
 	
 	@Test
